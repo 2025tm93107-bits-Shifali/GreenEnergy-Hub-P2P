4111d858d332809424135d2a25f09befd789eaf2
2025tm93107-bits-Shifali <2025tm93107@wilp.bits-pilani.ac.in>
2026-05-02 14:12:21 +0530

    Delete src directory

--- src/main/java/com/energyhub/repository/EnergyRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energyhub.repository;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.energyhub.model.EnergyRequest;
-
-
-public interface EnergyRequestRepository extends JpaRepository<EnergyRequest, Long> {
-    List<EnergyRequest> findByStatus(String status);
-}
